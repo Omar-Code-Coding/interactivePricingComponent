c351ee2fedd46163f1179fc02d66c5fb1a0c3945
Omar-Code-Coding <omaryasser_250@hotmail.com>
2021-11-10 18:00:07 +0200

    refactor: add new script file with refactored code

--- app/js/main.js
+++ app/js/main.js
@@ -1,72 +1,57 @@
 'use strict';
 
 const slider = document.getElementById('slider');
-const priceLabel = document.getElementById('price-label');
-const pageviewsLabel = document.getElementById('pageviews-label');
-const pricingInterval = document.getElementById('pricing-interval');
-const discountBtn = document.querySelector('#discount-btn');
-let discount = false;
-const pricesObj = {
-    price: [8, 12, 16, 24, 36],
-    pageview: ['10K', '50K', '100K', '500K', '1M'],
-    discountValue: 25,
-    discountPrice: [],
-    priceDiscount: function () {
-        for (const values of this.price) {
-            this.discountPrice.push(((values * 12) * this.discountValue) / 100);
-        }
-        return this.discountPrice;
+const priceLabel = document.querySelector('.price-component-slider__price-num');
+const pageviewsLabel = document.querySelector('.price-component-slider__pageviews');
+const pricingInterval = document.querySelector('.price-component-slider__price-label');
+const discountSwitch = document.querySelector('#discount-btn');
+
+class App {
+    #discount = false;
+    #discountValue = 25;
+    #prices = [8, 12, 16, 24, 36];
+    #pageviews = ['10K', '50K', '100K', '500K', '1M'];
+    #discountPrices;
+
+    constructor() {
+        slider.addEventListener('input', this._changeTrackColor.bind(this))
+        slider.addEventListener('input', this._renderPriceAndIntervalLabel.bind(this))
+        discountSwitch.addEventListener('input', this._toggleDiscount.bind(this));
+        this._setDiscountPrices()
     }
-};
-pricesObj.priceDiscount();
 
-/********************************* Event handlers ******************************/
-// Discount button
-discountBtn.addEventListener('input', discountToggle);
-
-// Slider Handler
-slider.addEventListener('input', () => {
-    sliderTrack();
-    sliderLabel(priceLabel, pricingInterval, pageviewsLabel);
-});
-init();
-
-// Intializing Default text
-function init() {
-    priceLabel.textContent = '$16.00';
-    pageviewsLabel.textContent = '100K pageviews';
-    pricingInterval.textContent = '/ month';
-};
-
-// Discount button
-function discountToggle() {
-    discount = discount === false ? true : false;
-    pricingInterval.textContent = '/ year';
-    definePriceAndInterval(priceLabel, pricingInterval);
-};
-
-
-// Change the Slider's tralling progress bar's color
-function sliderTrack() {
-    const sliderValue = slider.value * 25;
-    const color = `linear-gradient(90deg, hsl(174, 77%, 80%) ${sliderValue}%,
-    hsl(224, 65%, 95%) ${sliderValue}%)`;
-    return slider.style.background = color;
-};
-
-// Change Slider related labels (Price, Pageviews)
-function sliderLabel(el1, el2, el3) {
-    definePriceAndInterval(el1, el2);
-    el3.textContent = `${pricesObj.pageview[slider.value]} pageviews`;
-};
-
-// Check if Discount button was pressed
-function definePriceAndInterval(el1, el2) {
-    if (discount !== true) {
-        el1.textContent = `$${pricesObj.price[slider.value].toFixed(2)}`;
-        el2.textContent = '/ month';
-    } else {
-        el1.textContent = `$${pricesObj.discountPrice[slider.value].toFixed(2)}`;
-        el2.textContent = '/ year';
+    // Change track's trailing color based on the value of the slider
+    _changeTrackColor() {
+        const mult = 25;
+        const color = `
+            linear-gradient(90deg, hsl(174, 77%, 80%) ${slider.value * mult}%,
+            hsl(224, 65%, 95%) ${slider.value * mult}%)
+        `;
+        return slider.style.background = color;
     }
-};
+
+    // Define discount if the discountSwitch is clicked
+    _toggleDiscount() {
+        this.#discount = this.#discount === false ? true : false;
+        this._renderPriceAndIntervalLabel();
+    }
+
+    // Render the price and interval
+    _renderPriceAndIntervalLabel() {
+        const interval = this.#discount ? '/ year' : '/ month';
+        const price = this.#discount ? this.#discountPrices : this.#prices;
+
+        priceLabel.textContent = `$${price[slider.value].toFixed(2)}`;
+        pricingInterval.textContent = interval;
+        pageviewsLabel.textContent = `${this.#pageviews[slider.value]} pageviews`;
+    }
+
+    // Set discountPrice's array values
+    _setDiscountPrices() {
+        return this.#discountPrices = this.#prices.map(
+            i => ((i * 12) * this.#discountValue) / 100
+        );
+    }
+}
+
+const app = new App();
