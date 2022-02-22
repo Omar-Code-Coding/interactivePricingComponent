bbb0526ae4c42f2c21c901cb02381ceab2e50315
Omar-Code-Coding <omaryasser_250@hotmail.com>
2022-02-22 18:02:17 +0200

    fix: slider not styling properly

--- app/js/main.js
+++ app/js/main.js
@@ -5,25 +5,31 @@ const priceLabel = document.querySelector('.price-component-slider__price-num');
 const pageviewsLabel = document.querySelector('.price-component-slider__pageviews');
 const pricingInterval = document.querySelector('.price-component-slider__price-label');
 const discountSwitch = document.querySelector('#discount-btn');
+const plans = {
+    '10K': 8,
+    '50K': 12,
+    '100K': 16,
+    '500K': 24,
+    '1M': 36
+};
 
 class App {
     #discount = false;
     #discountValue = 25;
-    #pricesAndPageviews = {
-        '10K': 8,
-        '50K': 12,
-        '100K': 16,
-        '500K': 24,
-        '1M': 36
-    };
-    // #prices = [8, 12, 16, 24, 36];
-    // #pageviews = ['10K', '50K', '100K', '500K', '1M'];
-    // #discountPrices;
+    #plans = new Map();
 
     constructor() {
+        // Elements
+
+        // Events
         slider.addEventListener('input', this._changeTrailingTrackColor.bind(this))
-        slider.addEventListener('input', this._renderPriceAndIntervalLabel.bind(this))
+        slider.addEventListener('input', this._renderLabels.bind(this))
         discountSwitch.addEventListener('input', this._toggleDiscount.bind(this));
+    }
+
+    _setPlans(plansObj) {
+        const plans = Object.entries(plansObj);
+        plans.forEach(([key, value]) => this.#plans.set(key, value));
     }
 
     // Change trailing track's color based on the value of the slider
@@ -36,43 +42,39 @@ class App {
         return slider.style.background = color;
     }
 
-    // Define discount if the discountSwitch is On
+    // Define discount if discount toggle is On
     _toggleDiscount() {
         this.#discount = this.#discount === false ? true : false;
-        this._renderPriceAndIntervalLabel();
+        this._renderLabels();
     }
 
-    // Render the price and interval
-    _renderPriceAndIntervalLabel() {
+    // Render plan and interval
+    _renderLabels() {
+        this._renderInterval();
+        this._renderPlan()
+    }
+
+    _renderPlan() {
         const discountPrice = i => ((i * 12) * this.#discountValue) / 100;
 
-        // Set interval's value if "#discount" is true or false
-        const interval = this.#discount ? '/ year' : '/ month';
-
         // Change pageviews's value based on the Slider
-        const pageviews = Object.keys(this.#pricesAndPageviews)[slider.value];
+        const pageviews = [...this.#plans.keys()][slider.value];
+        pageviewsLabel.textContent = `${pageviews} pageviews`;
 
-        // Access price in "#pricesAndPageviews" based on "pageviews" value
-        const price = this.#pricesAndPageviews[`${pageviews}`];
+        // Access price in "#plans" based on "pageviews" value
+        const price = this.#plans.get(pageviews);
 
         // Discount the price if "#discount" is true, else the price is normal
         const definePrice = this.#discount ? discountPrice(price) : price;
-
-        pricingInterval.textContent = interval;
         priceLabel.textContent = `$${definePrice.toFixed(2)}`;
-        pageviewsLabel.textContent = `${pageviews} pageviews`;
-
-        // const price = this.#discount ? this.#discountPrices : this.#prices;
-        // priceLabel.textContent = `$${price[slider.value].toFixed(2)}`;
-        // pageviewsLabel.textContent = `${this.#pageviews[slider.value]} pageviews`;
     }
 
-    // Set discountPrice's array values
-    // _setDiscountPrices() {
-    //     return this.#discountPrices = this.#prices.map(
-    //         i => ((i * 12) * this.#discountValue) / 100
-    //     );
-    // }
+    _renderInterval() {
+        // Set interval's value if "#discount" is true or false
+        const interval = this.#discount ? '/ year' : '/ month';
+        pricingInterval.textContent = interval;
+    }
 }
 
 const app = new App();
+app._setPlans(plans)
